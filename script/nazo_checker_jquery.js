56e1febd11fcfadae9945afc5b8d3c0b0a2c0909
miura biwa <nyancorori0w0@gmail.com>
2020-08-04 08:34:36 +0900

    fix indent & add try..catch

--- script/nazo_checker_jquery.js
+++ script/nazo_checker_jquery.js
@@ -1,29 +1,33 @@
 function AnswerCheck(form){
-  // 入力欄に入っている文字列を拾う
-  send_text = $(form).children("input[type=text]")[0].value;
-  // 入力欄が空のとき、送信せず、エラーメッセージを出す
-  if(send_text.length == 0){
-    $("#result").text("入力欄が空です");
-    return false;
-  }
+    try{
+        // 入力欄に入っている文字列を拾う
+        send_text = $(form).children("input[type=text]")[0].value;
+        // 入力欄が空のとき、送信せず、エラーメッセージを出す
+        if(send_text.length == 0){
+            $("#result").text("入力欄が空です");
+            return false;
+        }
 
-  $.ajax({
-    url: "answers/"+send_text,
-    type: "GET",
-    dataType: "json",
-    timeout: 3000,
-  })
-  .done(function(resp){
-    // answers/{send_text}が存在する
-    if(resp["type"] == "move"){  // 遷移
-      window.location.href = resp["value"];
-    }else if(resp["type"] == "alert"){  // アラート
-      alert(resp["value"])
+        $.ajax({
+            url: "answers/"+send_text,
+            type: "GET",
+            dataType: "json",
+            timeout: 3000,
+        })
+        .done(function(resp){
+            // answers/{send_text}が存在する
+            if(resp["type"] == "move"){  // 遷移
+                window.location.href = resp["value"];
+            }else if(resp["type"] == "alert"){  // アラート
+                alert(resp["value"])
+            }
+        })
+        .fail(function(){
+            // answers/{send_text}が存在しない
+            $("#result").text("「"+send_text+"」は不正解です");
+        })
+    } catch(error){
+        console.error(error);
     }
-  })
-  .fail(function(){
-    // answers/{send_text}が存在しない
-    $("#result").text("「"+send_text+"」は不正解です");
-  })
 
 }
--- script/nazo_checker_plain.js
+++ script/nazo_checker_plain.js
@@ -1,37 +1,41 @@
 function AnswerCheck(form){
-  // 入力欄に入っている文字列を拾う
-  send_text = form.firstElementChild.value;
-  // 入力欄が空のとき、送信せず、エラーメッセージを出す
-  if(send_text.length == 0){
-    result.innerHTML = "入力欄が空です";
-    return false;
-  }
+    try{
+        // 入力欄に入っている文字列を拾う
+        send_text = form.firstElementChild.value;
+        // 入力欄が空のとき、送信せず、エラーメッセージを出す
+        if(send_text.length == 0){
+            result.innerHTML = "入力欄が空です";
+            return false;
+        }
 
-  // XMLHttpRequestオブジェクトを生成する
-  var req = new XMLHttpRequest();
-  // タイムアウトを3000ミリ秒に設定する
-  req.timeout = 3000;
-  // レスポンスはJSONで受け取る
-  req.responseType = "json";
+        // XMLHttpRequestオブジェクトを生成する
+        var req = new XMLHttpRequest();
+        // タイムアウトを3000ミリ秒に設定する
+        req.timeout = 3000;
+        // レスポンスはJSONで受け取る
+        req.responseType = "json";
 
-  // レスポンスを受け取った後の処理を定義する
-  req.addEventListener("loadend", function(){
-    if(req.status == 200){
-      // answers/{send_text}が存在する
-      if(req.response["type"] == "move"){  // 遷移
-        window.location.href = req.response["value"];
-      }else if(req.response["type"] == "alert"){  // アラート
-        alert(req.response["value"]);
-      }
-    }else{
-      // answers/{send_text}が存在しない
-      let result = document.getElementById("result");
-      result.innerHTML = "「"+send_text+"」は不正解です";
+        // レスポンスを受け取った後の処理を定義する
+        req.addEventListener("loadend", function(){
+            if(req.status == 200){
+                // answers/{send_text}が存在する
+                if(req.response["type"] == "move"){  // 遷移
+                    window.location.href = req.response["value"];
+                }else if(req.response["type"] == "alert"){  // アラート
+                    alert(req.response["value"]);
+                }
+            }else{
+                // answers/{send_text}が存在しない
+                let result = document.getElementById("result");
+                result.innerHTML = "「"+send_text+"」は不正解です";
+            }
+        })
+
+        req.open("GET", "answers/"+send_text, true);
+        // 送信
+        req.send();
+    }catch(error){
+        console.error(error);
     }
-  })
-
-  req.open("GET", "answers/"+send_text, true);
-  // 送信
-  req.send();
 
 }
